b69cd7168a3b36af53c59932bd354c2a29776358
BatuhanCaglar <fbmanbc@gmail.com>
2025-02-05 14:00:35 -0500

    bro

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -6,11 +6,20 @@ import TheWelcome from '../components/TheWelcome.vue'
   <main>
     <h1>Make-A-Weapon</h1>
     <TheWelcome />
+    <div class="bdev">
+      <h2>The Blacksmith</h2>
+    </div>
   </main>
 </template>
 
 <style scoped>
 h1 {
+  text-align: center;
+  font-family: 'Handjet', sans-serif;
+  font-optical-sizing: auto;
+}
+
+h2 {
   text-align: center;
   font-family: 'Handjet', sans-serif;
   font-optical-sizing: auto;
